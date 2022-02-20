b7b981ddf69aa121331723b31860b70d5428198d
sherin <vsherin@gmail.com>
2022-02-20 18:54:47 +0530

    Updates

---
+++ precache-manifest.971e9f0a40b57d3acafd1d6dbf875a47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed80717bef793ed92945029652ef8dc",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "b16b4b0807a68b22bc78",
+    "url": "/login/static/css/main.86369865.chunk.css"
+  },
+  {
+    "revision": "07e84d679aaf8e736593",
+    "url": "/login/static/js/2.84b84c51.chunk.js"
+  },
+  {
+    "revision": "b16b4b0807a68b22bc78",
+    "url": "/login/static/js/main.d04a05d0.chunk.js"
+  },
+  {
+    "revision": "d036c0eadd0ee639ddc1",
+    "url": "/login/static/js/runtime~main.299f3dfc.js"
+  }
+]);
